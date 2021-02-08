5e7cf72c55c8e8216a7507d0c88a249fcffbbbf3
Azhar Uddin Sheikh <Azharthegreat123@gmail.com>
2021-02-08 20:33:55 +0530

    created another model name offer

--- product/models.py
+++ product/models.py
@@ -8,3 +8,9 @@ class Product(models.Model):
     price = models.FloatField()
     stocks = models.IntegerField()
     image = models.CharField(max_length=2083)
+
+
+class Offer(models.Model):
+    code = models.CharField(max_length=10)
+    description = models.CharField(max_length=500)
+    discount = models.FloatField()
